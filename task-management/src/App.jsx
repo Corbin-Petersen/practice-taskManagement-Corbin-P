caf5892ef99a61dc50b9663d98f894c045c0793d
Corbin-Petersen <corbincpetersen@gmail.com>
2025-05-15 09:34:51 -0500

    initial setup after install vite + react and react-router, removed default content

---
+++ task-management/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
